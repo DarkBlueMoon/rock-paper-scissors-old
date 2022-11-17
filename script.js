1a18dc66d2fc19ea2ace67cc823bee0e1cbbfa07
Demetri Evans <3819574+DarkBlueMoon@users.noreply.github.com>
2022-11-17 14:44:37 -0600

    Determine a winner from one round of RPS

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-alert("Hello world!");
+const choices = ["rock", "paper", "scissors"];
+
+function getComputerChoice() {
+  const randomChoice = choices[Math.floor(Math.random() * choices.length)];
+  return randomChoice;
+}
+
+function playRound(playerSelection, computerSelection) {
+  // compare selections: rock > scissors, scissors > paper, paper > rock
+
+  if (
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "scissors" && computerSelection === "paper") ||
+    (playerSelection === "paper" && computerSelection === "rock")
+  ) {
+    console.log(
+      `You win! ${capitalizeStr(playerSelection)} beats ${capitalizeStr(
+        computerSelection
+      )}!`
+    );
+  } else if (playerSelection === computerSelection) {
+    console.log("It's a draw!");
+  } else {
+    console.log(
+      `You lose! ${capitalizeStr(computerSelection)} beats ${capitalizeStr(
+        playerSelection
+      )}!`
+    );
+  }
+}
+
+function capitalizeStr(str) {
+  return str[0].toUpperCase() + str.slice(1);
+}
+
+const playerSelection = "RoCk";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection.toLowerCase(), computerSelection));
