96c99a0123e4e116afb87d59fd2eba83b1972e7d
Demetri Evans <3819574+DarkBlueMoon@users.noreply.github.com>
2022-11-20 22:20:33 -0600

    Add UI for game

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
 let playerScore = 0;
 let computerScore = 0;
+const choices = ["rock", "paper", "scissors"];
+const buttonsDiv = document.querySelector(".buttons");
+const MAX_ROUNDS = 5;
+let currentRound = 1;
+const resultsDiv = document.querySelector(".results");
+let running = true;
+
+buttonsDiv.addEventListener("click", (e) => {
+  const clickedTarget = e.target;
+  if (clickedTarget.tagName === "BUTTON" && running) {
+    playRound(
+      clickedTarget.textContent.toLowerCase(),
+      getComputerChoice(choices)
+    );
+  }
+});
+
+function resetGame() {
+  playerScore = 0;
+  computerScore = 0;
+  currentRound = 1;
+  running = true;
+
+  while (resultsDiv.hasChildNodes()) {
+    resultsDiv.removeChild(resultsDiv.firstChild);
+  }
+}
 
 function getComputerChoice(choices) {
   const randomChoice = choices[Math.floor(Math.random() * choices.length)];
@@ -13,12 +40,14 @@ function updateScore(winner) {
 
 function displayWinner() {
   if (playerScore > computerScore) {
-    console.log("Player wins!");
+    createResult("Player wins!");
   } else if (computerScore > playerScore) {
-    console.log("Computer wins!");
+    createResult("Computer wins!");
   } else {
-    console.log("Tie game!");
+    createResult("Tie game!");
   }
+
+  createButton("Reset Game");
 }
 
 function isValidChoice(choice) {
@@ -29,44 +58,60 @@ function capitalizeStr(str) {
   return str[0].toUpperCase() + str.slice(1);
 }
 
+function createResult(text) {
+  const result = document.createElement("p");
+  result.textContent = text;
+  appendResult(resultsDiv, result);
+}
+
+function createButton(text) {
+  const btn = document.createElement("button");
+  btn.textContent = text;
+  createResetListener(btn);
+  appendResult(resultsDiv, btn);
+}
+
+function createResetListener(btn) {
+  btn.addEventListener("click", resetGame);
+}
+
+function appendResult(container, result) {
+  container.appendChild(result);
+}
+
+function checkForGameOver() {
+  if (currentRound < MAX_ROUNDS) {
+    currentRound++;
+    return;
+  }
+
+  running = false;
+  displayWinner();
+}
+
 function playRound(playerSelection, computerSelection) {
   // compare selections: rock > scissors, scissors > paper, paper > rock
-
   if (
     (playerSelection === "rock" && computerSelection === "scissors") ||
     (playerSelection === "scissors" && computerSelection === "paper") ||
     (playerSelection === "paper" && computerSelection === "rock")
   ) {
     updateScore("player");
-    return `You win! ${capitalizeStr(playerSelection)} beats ${capitalizeStr(
-      computerSelection
-    )}!`;
+    createResult(
+      `You win! ${capitalizeStr(playerSelection)} beats ${capitalizeStr(
+        computerSelection
+      )}!`
+    );
   } else if (playerSelection === computerSelection) {
-    return "It's a draw!";
+    createResult("It's a draw!");
   } else {
     updateScore("computer");
-    return `You lose! ${capitalizeStr(computerSelection)} beats ${capitalizeStr(
-      playerSelection
-    )}!`;
-  }
-}
-
-function game() {
-  const choices = ["rock", "paper", "scissors"];
-
-  for (let i = 0; i < 5; i++) {
-    let playerSelection = prompt("Rock, Paper, or Scissors?");
-
-    while (!isValidChoice(playerSelection.toLowerCase())) {
-      playerSelection = prompt("Rock, Paper, or Scissors?");
-    }
-
-    const computerSelection = getComputerChoice(choices);
-
-    console.log(playRound(playerSelection.toLowerCase(), computerSelection));
+    createResult(
+      `You lose! ${capitalizeStr(computerSelection)} beats ${capitalizeStr(
+        playerSelection
+      )}!`
+    );
   }
 
-  displayWinner();
+  checkForGameOver();
 }
-
-game();
